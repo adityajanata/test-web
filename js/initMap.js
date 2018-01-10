b2af614d680ff1fa91807996008b2d8c099a7124
Aditya Janata Priya <adityajanata@ymail.com>
2018-01-11 05:02:38 +0700

    first

---
+++ js/initMap.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: -6.238279, lng: 106.975564};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
---
+++ js/navbarclick.js
@@ -0,0 +1,17 @@
+$(function () {
+        $("[href^='#']").on("click", function (e)  {
+             var target = $(this).attr('href');
+
+              var scrollTop = $( target ).offset().top-$('.header').height()-$('.header').outerHeight();
+
+              if ( target =='#home'){
+                 scrollTop = 0;
+              }
+               
+              $("body, html").animate({
+                      scrollTop: scrollTop
+                  }, 500);
+
+              e.preventDefault();
+        });
+    });
